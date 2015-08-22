bc46e252257b1cd8dc21098b7c4041323d324efc
Robin-Zhu <liboefz@163.com>
2015-08-22 21:18:06 +0800

    demo1

--- demo.py
+++ demo.py
@@ -3,9 +3,18 @@
 import sys
 import os
 
+def choose(my_region, rival_region, all_cards, cards_in_hands):
+    
+    return 1, 2
+
 if __name__ == '__main__':
+    my_region = [[] for i in range(9)]
+    rival_region = [[] for i in range(9)]
+
+    all_cards = {'A': [1]*10, 'B': [1]*10, 'C': [1]*10, 'D': [1]*10, 'E': [1]*10, 'F': [1]*10}
+
     cards_in_hands = []
-    status = [[] for i in range(9)]
+
     
     while True:
         n = int(sys.stdin.readline())
@@ -13,10 +22,14 @@ if __name__ == '__main__':
         over = False
         for i in range(n):
             cmd = sys.stdin.readline()
-            #sys.stderr.write("demo %d cmd: " % os.getpid()+ cmd)
+            sys.stderr.write("demo %d cmd: " % os.getpid()+ cmd)
             items = cmd.split()
             if items[0] == 'cardget':
                 cards_in_hands.append(items[1])
+                color = items[1][0]
+                num = int(items[1][1])
+                all_cards[color][num-1] = 0
+
             elif items[0] == 'rival':
                 pass
             else:
@@ -24,11 +37,17 @@ if __name__ == '__main__':
         if over:
             break
         else:
+
+            
             for i in range(9):
-                if len(status[i]) < 3:
-                    region = i
-                    status[i].append(cards_in_hands[0])
+                if len(my_region[i]) < 3:
+                    reg_idx = i
+                    my_region[i].append(cards_in_hands[0])
                     break
-            sys.stdout.write("act %d %s\n" % (region, cards_in_hands[0]))
+
+            i, j = choose(my_region, rival_region, all_cards, cards_in_hands)                
+            sys.stdout.write("act %d %s\n" % (reg_idx, cards_in_hands[0]))
             sys.stdout.flush()
+            sys.stderr.flush()            
             cards_in_hands = cards_in_hands[1:]
+            
